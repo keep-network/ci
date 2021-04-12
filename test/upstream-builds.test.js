fa4b6b469cc4aeca30eaf11d6ab967b31b46cc3b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-04-12 15:47:20 +0200

    Function to validate upstream builds against JSON schema

---
+++ test/upstream-builds.test.js
@@ -0,0 +1,43 @@
+import { expect } from "chai"
+
+import { validateUpstreamBuilds } from "../lib/upstream-builds.js"
+
+describe("Upstream Builds", () => {
+  describe("validateUpstreamBuilds", () => {
+    it("succeeds for array of two", async () => {
+      const { isValid, errors } = validateUpstreamBuilds(
+        `[{"module":"a","ref":"b","version":"c","url":"d"},{"module":"a2","ref":"b","version":"c","url":"d"}]`
+      )
+
+      expect(isValid).to.be.true
+      expect(errors).to.be.undefined
+    })
+
+    it("succeeds for empty array", async () => {
+      const { isValid, errors } = validateUpstreamBuilds("[]")
+
+      expect(isValid).to.be.true
+      expect(errors).to.be.undefined
+    })
+
+    it("fails for object", async () => {
+      const { isValid, errors } = validateUpstreamBuilds(
+        '{"module":"a","ref":"b"}'
+      )
+
+      expect(isValid).to.be.false
+      expect(errors).to.have.lengthOf(1)
+      expect(errors[0]).property("message", "is not of a type(s) array")
+    })
+
+    it("fails for missing required parameter", async () => {
+      const { isValid, errors } = validateUpstreamBuilds(
+        '[{"module":"a","ref":"b","version":"c"}]'
+      )
+
+      expect(isValid).to.be.false
+      expect(errors).to.have.lengthOf(1)
+      expect(errors[0]).property("message", 'requires property "url"')
+    })
+  })
+})
